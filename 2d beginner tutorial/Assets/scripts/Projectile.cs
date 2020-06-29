0e1f768c578b6e31b41e151c1367444371e7f053
shailpratapsinghchandel <60536036+shailpratapsinghchandel@users.noreply.github.com>
2020-06-29 20:53:15 +0530

    game
    summary

---
+++ 2d beginner tutorial/Assets/scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //we also add a debug log to know what the projectile touch
+        Debug.Log("Projectile Collision with " + other.gameObject);
+        Destroy(gameObject);
+    }
+}
